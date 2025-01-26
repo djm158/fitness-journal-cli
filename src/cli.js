8f2240e35ba0e22f9841de3acaaca2100bafc362
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-01-26 09:12:33 -0500

    add parsing for workout types

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import { input, checkbox } from "@inquirer/prompts";
+import { input, checkbox, Separator } from "@inquirer/prompts";
 import fs from "fs/promises";
 import path from "path";
+import chalk from "chalk";
 import { promptForConfig } from "./prompts/config.js";
 import { defaultConfig, defaultConfigPath } from "./config.js";
 import { writeEntry } from "./writeEntry.js";
@@ -24,7 +25,6 @@ const hasConfigFile = await fs
   .catch(() => false);
 
 let config = defaultConfig;
-let journalPath = defaultConfig.journalPath;
 
 if (!hasConfigFile) {
   console.log("No config file found, creating one at ", defaultConfigPath);
@@ -59,21 +59,101 @@ const output = {
   workouts: {},
 };
 
+const workoutMap = {
+  Run: getRunInfo,
+  Bike: getBikeInfo,
+  Strength: getStrengthInfo,
+};
+
 for (const w of workouts) {
-  if (w === "Run") {
-    const distance = await input({
-      message: `Distance for ${w}?`,
-    });
-    details = await input({
-      message: `Details for ${w}?`,
-    });
-    output.workouts[w] = {
-      distance,
-      details,
-    };
+  if (workoutMap[w]) {
+    output.workouts[w] = await workoutMap[w](w);
   }
 }
 
 console.log(output);
 
-await writeEntry(config.journalPath, config, output);
+// await writeEntry(config.journalPath, config, output);
+
+async function getBikeInfo(w) {
+  return await getBasicDistanceDetails(w);
+}
+
+async function getRunInfo(w) {
+  return await getBasicDistanceDetails(w);
+}
+
+async function getStrengthInfo(w) {
+  const strengthExercises = await checkbox({
+    message: `Strength exercises?`,
+    choices: [
+      new Separator(chalk.bold("----- Upper Body -----")),
+      { value: "Bench Press" },
+      { value: "Pull Ups" },
+      { value: "Bicep Curls" },
+      { value: "Tricep Extensions" },
+      { value: "Push Ups" },
+      new Separator(chalk.bold("----- Core -----")),
+      { value: "Planks" },
+      { value: "Side Planks" },
+      { value: "Fire Hydrants" },
+      { value: "Balance Board" },
+      { value: "Single Leg Deadlift" },
+      { value: "Dead Bugs" },
+      { value: "Crab Walks" },
+      { value: "Mountain Climbers" },
+      { value: "V-Ups" },
+      { value: "Supermans" },
+      { value: "Russian Twists" },
+      { value: "Leg Raises" },
+      { value: "Flutter Kicks" },
+      { value: "Bicycles" },
+      { value: "Crunches" },
+      new Separator(chalk.bold("----- Lower Body -----")),
+      { value: "Squats" },
+      { value: "Single Leg Squats" },
+      { value: "Lunges" },
+      { value: "Deadlifts" },
+      { value: "Calf Raises" },
+      { value: "Pistol Squats" },
+    ],
+  });
+  const strengthExercisesInfo = {};
+  for (const e of strengthExercises) {
+    strengthExercisesInfo[e] = await getStrengthExerciseInfo(e);
+  }
+}
+
+async function getStrengthExerciseInfo(e) {
+  const sets = await input({
+    message: `${e} Sets?`,
+  });
+  const reps = await input({
+    message: `${e} Reps?`,
+  });
+  const weight = await input({
+    message: `${e} Weight?`,
+  });
+  const duration = await input({
+    message: `${e} Duration?`,
+  });
+  return {
+    sets,
+    reps,
+    duration,
+    weight,
+  };
+}
+
+async function getBasicDistanceDetails(w) {
+  const distance = await input({
+    message: `${w} Distance?`,
+  });
+  const details = await input({
+    message: `${w} Details?`,
+  });
+  return {
+    distance,
+    details,
+  };
+}
